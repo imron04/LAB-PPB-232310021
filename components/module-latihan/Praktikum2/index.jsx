6025bec01e5ab0bbf84f38733f6e5eb35a408fb6
unknown <imron0ganteng@gmail.com>
2026-04-09 13:19:22 +0700

    Tugas Praktikum-2

--- components/module-latihan/Praktikum2/index.jsx
+++ components/module-latihan/Praktikum2/index.jsx
@@ -1,36 +1,75 @@
 import { StyleSheet, Text, View } from "react-native";
 
 const Praktikum2 = () => {
+  // objek berisi data pribadi
   const personalData = {
     name: "Imron",
     tinggiBadan: 165,
     beratBadan: 50,
   };
+
+  // array of object berisi asupan harian
   const dailyPortion = [
     { waktu: "Sarapan", kalori: 400 },
     { waktu: "Lunch", kalori: 600 },
     { waktu: "Dinner", kalori: 500 },
     { waktu: "Cemilan Time", kalori: 100 },
   ];
+
+  // menjumlahkan total kalori menggunakan reduce
   const totalKalori = dailyPortion.reduce((acc, item) => {
     return acc + item.kalori;
   }, 0);
 
-  const tinggiBdn = personalData.tinggiBadan / 100;
-  const BMI = personalData.beratBadan / (tinggiBdn * tinggiBdn);
+  const tinggiBdn = personalData.tinggiBadan / 100; // Konversi tinggi badan ke meter
+  const BMI = personalData.beratBadan / (tinggiBdn * tinggiBdn); // hitung bmi
 
+  // status BMI berdasarkan hasil perhitungan
   const statusBMI = () => {
     if (BMI < 18.5) return "Kurus";
     if (BMI >= 18.5 && BMI <= 22.9) return "Ideal";
     return "Obesitas";
   };
 
+  // jumlah kalori harian yang diperlukan
   const calNeeded = personalData.beratBadan * 30;
 
+  // status kalori
   const statusKalori = () => {
     if (totalKalori < calNeeded - 100) return "Kurang";
     if (totalKalori > calNeeded + 100) return "Berlebih";
-    return "Ideal";
+    return "Cukup";
+  };
+
+  const kesimpulan = () => {
+    const bmi = statusBMI();
+    const kalori = statusKalori();
+
+    // Kesimpulan berdasarkan kesesuaian kondisi BMI dan asupan kalori
+    if (bmi == "Ideal" && kalori == "Cukup")
+      return "Berat badan anda ideal dan asupan kalori sudah terpenuhi";
+    if (bmi == "Ideal") {
+      if (kalori == "Berlebih")
+        return "Berat badan anda ideal tetapi asupan kalori berlebih";
+      if (kalori == "Kurang")
+        return "Berat badan anda ideal tetapi asupan kalori harian kurang";
+    }
+    if (bmi == "Kurus") {
+      if (kalori == "Berlebih")
+        return "Berat badan anda kurang dan asupan harian berlebih";
+      if (kalori == "Cukup")
+        return "Berat badan anda kurang tetapi asupan kalori harian cukup";
+      if (kalori == "Kurang")
+        return "Berat badan anda kurang dan asupan kalori harian juga kurang";
+    }
+    if (bmi == "Obesitas") {
+      if (kalori == "Berlebih")
+        return "Berat badan anda berlebihan dan asupan kalori harian juga berlebihan";
+      if (kalori == "Cukup")
+        return "Berat badan anda berlebihan dan asupan kalori cukup";
+      if (kalori == "Kurang")
+        return "Berat badan anda berlebihan dan asupan kalori harian kurang";
+    }
   };
 
   return (
@@ -55,10 +94,7 @@ const Praktikum2 = () => {
       <Text>BMI: {BMI.toFixed(2)}</Text>
       <Text>Status BMI: {statusBMI()}</Text>
 
-      <Text style={style.title}>Berat badan</Text>
-      <Text>
-        Berat badan {statusBMI()} dan asupan kalori {statusKalori()}
-      </Text>
+      <Text style={style.title}>{kesimpulan()}</Text>
     </View>
   );
 };
